44e467d7a12a2544070485d737b0175ea35f9519
Daniel Padilla <danperzipa@hotmail.com>
2015-09-24 10:24:06 -0500

    ....

--- app/src/main/java/com/ingdanielpadilla/ejemplo_fragment/MainActivity.java
+++ app/src/main/java/com/ingdanielpadilla/ejemplo_fragment/MainActivity.java
@@ -1,16 +1,26 @@
 package com.ingdanielpadilla.ejemplo_fragment;
 
+
+import android.net.Uri;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BlankFragment.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState==null){
+            getSupportFragmentManager().beginTransaction().add(R.id.container,new BlankFragment()).commit();
+        }
     }
 
     @Override
@@ -33,5 +43,11 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
